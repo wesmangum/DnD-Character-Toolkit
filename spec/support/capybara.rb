a51b95c2cacd1d2e9b0f4a90cc527eb9688539ba
wesmangum <wesmangum24@gmail.com>
2014-09-24 14:47:28 -0500

    fixed rake issues with javascript timeout

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -3,3 +3,7 @@ require 'capybara/poltergeist'
 
 Capybara.match = :prefer_exact
 Capybara.javascript_driver = :poltergeist
+options = {timeout: 60}
+Capybara.register_driver(:poltergeist) do |app|
+  Capybara::Poltergeist::Driver.new app, options
+end
